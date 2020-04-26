0ed0ba238d7861c2342a56f846dd6589b62f9ff6
David Andrews <dandrewsuk82@gmail.com>
2020-04-26 13:27:41 +0100

    Updates

---
+++ precache-manifest.4c7b0518c21a4bea9ac41e3089cd9b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ec707615a34e34423e009c74b21607",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "f0ac015f7783ca442797",
+    "url": "/scoreboard/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "948343b4e932bf535fc6",
+    "url": "/scoreboard/static/js/2.bae724c4.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/scoreboard/static/js/2.bae724c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ac015f7783ca442797",
+    "url": "/scoreboard/static/js/main.6cfc9b7d.chunk.js"
+  },
+  {
+    "revision": "ff9369c447f6cf4e574a",
+    "url": "/scoreboard/static/js/runtime-main.68683a59.js"
+  }
+]);
